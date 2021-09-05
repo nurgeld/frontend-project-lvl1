ffccb503ca1e561be256af80b4e550103e965a9f
Nurgeldy Dyussenov <nurgeldy@gmail.com>
2021-09-05 20:15:12 +0600

    fix logic in brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable linebreak-style */
+/* eslint-disable linebreak-style */
 
 import game from '../src/brain-even.js';
 
--- src/brain-even.js
+++ src/brain-even.js
@@ -1,36 +1,28 @@
+/* eslint-disable linebreak-style */
 import readlineSync from 'readline-sync';
-// Answer "yes" if the number is even, otherwise answer "no".
-// Question: 15
-// Your answer: yes
-// 'yes' is wrong answer ;(. Correct answer was 'no'.
-// Let's try again, Bill!
-export const greetUser = () => {
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}`);
-};
 
 const steps = 3;
-export const evenGame = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+export default () => {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}`);
+
+  const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+  console.log(task);
+
   let i = 0;
   while (i <= steps) {
     const randNumber = Math.floor(Math.random() * 1000);
     const correctAnswer = randNumber % 2 === 0 ? 'no' : 'yes';
+    console.log(`Question: ${randNumber}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-        console.log('Correct!');
+    if (Number(answer) === correctAnswer) {
+      console.log('Correct!');
     } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-        // console.log(`Let's try again, ${name}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
+    i += 1;
   }
-  const randNumber = Math.floor(Math.random() * 1000);
-  const correctAnswer = randNumber % 2 === 0 ? 'no' : 'yes';
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-    console.log('Let\'s try again,');
-  }
+  console.log(`Congratulations, ${name}!`);
 };
