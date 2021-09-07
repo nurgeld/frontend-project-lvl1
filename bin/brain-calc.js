40ea3303eec8a4b4f6aa95abeab1789c441eeb72
Nurgeldy Dyussenov <nurgeldy@gmail.com>
2021-09-07 23:09:43 +0600

    add game ingine logic

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* eslint-disable linebreak-style */
+
+import calcGame from '../src/games/brain-calc.js';
+
+calcGame();
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+const steps = 3;
+export default (game) => {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}`);
+
+  const [task, gameParts] = game;
+
+  console.log(task);
+
+  let i = 1;
+  while (i <= steps) {
+    const [question, correctAnswer] = gameParts();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    i += 1;
+  }
+  console.log(`Congratulations, ${name}!`);
+};
