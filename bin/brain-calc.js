2a821e0431acffebfd12f59c6b987b299af31867
Nurgeldy Dyussenov <nurgeldy@gmail.com>
2021-09-08 00:23:14 +0600

    trying solve module imports

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 /* eslint-disable linebreak-style */
 
-import calcGame from '../src/games/brain-calc.js';
+// import calcGame from '../src/games/brain-calc.js';
+import calcGame from '../src/brain-calc.js';
 
 calcGame();
