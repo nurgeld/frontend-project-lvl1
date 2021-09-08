6f1d0146201d2433698397d0be8427d3bf36030a
Nurgeldy Dyussenov <nurgeldy@gmail.com>
2021-09-08 21:27:03 +0600

    add rand func, brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* eslint-disable linebreak-style */
+
+import progressionGame from '../src/games/brain-progression.js';
+
+progressionGame();
---
+++ src/games/brain-progression.js
@@ -0,0 +1,27 @@
+import gameEngine from '../index.js';
+
+const task = 'What number is missing in the progression?';
+
+export const getElementOfProgression = (a, d, n) => a + ((n - 1) * d);
+
+export const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const progressionGameLogic = () => {
+  const firstElement = random(1, 15);
+  const step = random(1, 15);
+  const lengthOfProgression = random(5, 15);
+  const position = random(2, 10);
+  const answer = getElementOfProgression(firstElement, step, position);
+  const progression = [];
+  let i = 0;
+  while (i < lengthOfProgression) {
+    const element = i === position ? '..' : getElementOfProgression(firstElement, step, i);
+    progression.push(element);
+    i += 1;
+  }
+  const question = progression.join(' ');
+
+  return [question, String(answer)];
+};
+
+export default () => gameEngine([task, progressionGameLogic]);
