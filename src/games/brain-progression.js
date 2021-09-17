c1f833e215489f7eceb90609ff14a290cd56f10c
Nurgeldy Dyussenov <nurgeldy@gmail.com>
2021-09-18 00:11:32 +0600

    refactoring brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,17 +5,19 @@ const task = 'What number is missing in the progression?';
 
 const calculateElementOfProgression = (first, difference, index) => first + index * difference;
 
-const generateProgression = (first, difference, progressionLength, indexToMask = null) => {
+const generateProgression = (first, difference, progressionLength) => {
   const progression = [];
   for (let n = 0; n < progressionLength; n += 1) {
     const nthTerm = first + n * difference;
     progression.push(nthTerm);
   }
-  if (indexToMask !== null) {
-    progression[indexToMask] = '..';
-    return progression.join(' ');
-  }
-  return progression.join(' ');
+  return progression;
+};
+
+const maskElement = (coll, index) => {
+  const result = coll;
+  result[index] = '..';
+  return result;
 };
 
 export const runGameRound = () => {
@@ -23,7 +25,9 @@ export const runGameRound = () => {
   const difference = getRandomNumber(1, 15);
   const progressionLength = getRandomNumber(5, 15);
   const indexOfMaskedElement = getRandomNumber(0, progressionLength - 1);
-  const question = generateProgression(first, difference, progressionLength, indexOfMaskedElement);
+  const progression = generateProgression(first, difference, progressionLength);
+  const maskedProgression = maskElement(progression, indexOfMaskedElement);
+  const question = maskedProgression.join(' ');
   const answer = calculateElementOfProgression(first, difference, indexOfMaskedElement);
 
   return [question, String(answer)];
